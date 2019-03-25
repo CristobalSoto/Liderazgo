14febb90c5227aba84ec27f3c2ca6669454e19bc
CristobalSoto <cristobal@onesla.com>
2019-03-25 11:58:46 -0300

    Updates

---
+++ precache-manifest.2672360cc9703ac5725c3c4aa88db85b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dad117b926a9b1f0a49e263d0be97a5",
+    "url": "/Liderazgo/static/media/jugador_1.0dad117b.png"
+  },
+  {
+    "revision": "c235e4a22934d344686f867a612e1c7b",
+    "url": "/Liderazgo/static/media/bg-situation.c235e4a2.png"
+  },
+  {
+    "revision": "44c6656556c115eaf681",
+    "url": "/Liderazgo/static/js/runtime~main.87d1b5a1.js"
+  },
+  {
+    "revision": "36ca0360df4a160b26fb",
+    "url": "/Liderazgo/static/js/main.899ba0d6.chunk.js"
+  },
+  {
+    "revision": "8124d38033f768b4dde3",
+    "url": "/Liderazgo/static/js/2.4d9fcf74.chunk.js"
+  },
+  {
+    "revision": "36ca0360df4a160b26fb",
+    "url": "/Liderazgo/static/css/main.b8bb03ad.chunk.css"
+  },
+  {
+    "revision": "88d4aed5b3d1e7ff3b694009f75df996",
+    "url": "/Liderazgo/index.html"
+  }
+];
